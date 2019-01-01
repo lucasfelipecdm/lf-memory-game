59da0c8743ee4ab71af950a5e8cfe1797c84d840
Lucas Felipe Costa da Mata <lucasfelipecdm@gmail.com>
2018-12-31 22:43:35 -0200

    feat: Adds comments, final changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+/*[en] - Init variables declaration
+ Create an array with all icons (card symbols)
+ [pt-br] - Início da declaração de variáveis
+ Criando uma lista com todos os icones (símbolos das cartas)*/
  const cardsArray = [
     'fa-money',
     'fa-money',
@@ -16,16 +20,32 @@
     'fa-headphones',
     'fa-headphones'
 ];
+ /* [en] You can change the symbol of cards just changing the "fa icon" name in cardsArray
+  EXAMPLE: 'fa-fire'
+  But remember, you need a even number of cards.
+  [pt-br] Você pode mudar o símbolo da carta apenas mudando o nome do "fa icon" na cardsArray
+  EXEMPLO: 'fa-fire'
+  Mas lembre-se, você precisa de um numero par de cartas.*/
  let lockBoard = false;
  let [firstClick, secondClick] = [null, null];
  let [fcli, scli] = [null, null];
  let endScore = 0;
+ //move(s) variables
  let moves = 0;
  let itMatches = 0;
+ // star icon variable
  const allStars = $('.fa-star');
+ // Time variables
  let startGame = 0;
  let gameInterval;
-  
+ /*[en] - End variables declaration
+  [ptbr] - Fim da declaração de variáveis*/
+ 
+ /*[en] - Init function declaration
+  [ptbr] - Inicio da declaração de funções*/
+ 
+ /*[en] - Shuffle function from http://stackoverflow.com/a/2450976
+  [ptbr] - Função de embaralhar adaptado de http://stackoverflow.com/a/2450976*/
  function shuffle(array) {
      let currentIndex = array.length,
          temporaryValue, randomIndex;
@@ -41,7 +61,8 @@
      return array;
  }
  
- 
+ /*[en] - Timer inspired by https://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer
+  [ptbr] - Temporizador adaptado de https://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer*/
  function timer() {
      let minutes = 0;
      let seconds = 0;
@@ -59,24 +80,28 @@
      }, 1000);
  }
  
- 
+ /*[en] - Flip card and show their symbol
+  [ptbr] - Gira a carta e mostra seu simbolo*/
  function displaySymbol(cardOpen) {
      $(cardOpen).addClass("open show");
  }
  
- 
+ /*[en] - Back card to default position when doesn't match
+  [ptbr] - Volta a carta para a posição padrão quando não são iguais*/
  function closeUnmatchedCards() {
      setTimeout(function () {
          $('.unMatch').removeClass('unMatch show open');
      }, 100);
  }
  
- 
+ /*[en] - Set the firstClick and secondClick value to null !IMPORTANT! the game will crash if you don't set this values
+  [ptbr] - Adiciona o valor null em firstClick e secondClick !IMPORTANTE! o jogo travará caso você não adicionar esses valores*/
  function restartClick() {
      [firstClick, secondClick] = [null, null]
  }
  
- 
+ /*[en] - Set the number of stars show
+  [ptbr] - Posiciona o numero de estrelas*/
  function setStarsNum() {
      if (moves === 16) {
          $(allStars[0]).addClass('hide');
@@ -87,7 +112,8 @@
      }
  }
  
- 
+ /*[en] - Increments the move counter and call the setStarsNum function
+  [ptbr] - Incrementa o contador de movimentos e chama a função setStarsNum*/
  function moveCounter() {
      moves++;
      $('.moves').html(moves);
@@ -97,7 +123,8 @@
      }
  }
  
- 
+ /*[en] - Show all stars, clean the variable itMatches, set startGame and moves to 0, set values of fcli and scli to null and hide the modal
+  [ptbr] - Mostra todas as estrelas, limpa a variavel itMatches, adiciona o valor 0 ao startGame e moves, adiciona valor null a fcli e scli e esconde o modal */
  function cleanStatus() {
      for (let i = 0; i < allStars.length; i++) {
          $(allStars[i]).removeClass('hide');
@@ -111,12 +138,14 @@
      $('.block-screen').removeClass('showed').addClass('hide');
  }
  
- 
+ /*[en] - Creates the element "li",  add class "card", creates the element "i" within "li", add class "fa" + "cardIcon" and return it
+  [ptbr] - Cria o elemento "li" adiciona a class "card", cria o elemento dentro de li e adiciona a class "fa" + "cardIcon" e o retorna*/
  function newCard(cardIcon) {
      return '<li class ="card"><i class="fa ' + cardIcon + '"></i></li>'
  };
  
- 
+ /*[en] - Adds "match" and "true" class to corresponding cards, increments endScore and checks that all cards have been combined
+  [ptbr] - Adiciona as classes "match" e "true" as cartas combinadas, incrementa o endScore e verifica se todas as cartas foram combinadas*/
  function matchCard() {
      $(fcli).addClass('match true');
      $(scli).addClass('match true');
@@ -130,7 +159,8 @@
      lockBoard = false;
  }
  
- 
+ /*[en] - Adds class "unMatch" to fcli and scli, decrement endScore and call closeUnmatchedCards in setTimeout of 750ms
+  [ptbr] - Adiciona a classe "unMatch" em fcli e scli, decrementa o endScore and chama a função closeUnmatchedCards em setTimeout com um tempo de 750ms */
  function unMatchCard() {
      $(fcli).addClass('unMatch');
      $(scli).addClass('unMatch');
@@ -141,7 +171,8 @@
      lockBoard = false;
  }
  
- 
+ /*[en] - Creates an event listener to card, verify startGame to call the timer, verify lockBoard and if the card is already open, then verify if its the first card or the second card and call the other functions
+  [ptbr] - Cria um observador de evento para a carta, verifica o startGame para chamar a função de temporizador, verifica o lockBoard e se a carta já esta aberta, depois verifica se é o primeiro card ou o segundo e chama suas funções*/
  function cardClick(card) {
      $(card).on('click', function () {
          if (startGame === 0) {
@@ -172,7 +203,8 @@
      })
  }
  
- 
+ /*[en] - Init the game, clean all status and set all variable values to default, create the "li" element with the function newCard and append it to HTML document then call the cardClick to create the event listeners on cards
+  [ptbr] - Inicia o jogo, limpa todos os status e adiciona os valores padrões a todas as variaveis, cria o elemento "li" usando a função newCard e adiciona ao documento HTML, depois chama a função cardClick para criar os observadores de eventos*/
  function gameStart() {
      cleanStatus();
      restartClick();
@@ -188,17 +220,20 @@
  }
  gameStart();
  
- 
+ /*[en] - Creates an event listeners to call gameStart when the element is clicked
+  [ptbr] - Cria um observador de eventos para chamar a função gameStart quando o elemento é clicado*/
  $('.restart').on('click', function () {
      gameStart();
  });
  
- 
+ /*[en] - Creates an event listeners to call gameStart when the element is clicked
+  [ptbr] - Cria um observador de eventos para chamar a função gameStart quando o elemento é clicado*/
  $('.btn-yes').on('click', function () {
      gameStart();
  });
  
- 
+ /*[en] - Creates an event listeners to hidden the modal when clicked
+  [ptbr] - Cria um observador de eventos para esconder o modal quando o elemento é clicado*/
  $('.btn-no').on('click', function () {
      $('.block-screen').removeClass('showed').addClass('hide');
  });
